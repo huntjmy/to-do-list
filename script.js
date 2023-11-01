21594a151d2b5524e9a1842bf37c01bf91954332
mnnyng <huntjmy@naver.com>
2023-11-01 11:16:05 +0900

    commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
-function addTask() {
+const addTask = () => {
     if (inputBox.value === '') {
         alert('할 일을 적어주세요 !');
     } else {
@@ -14,11 +14,11 @@ function addTask() {
     }
     inputBox.value = ''; // 할 일을 추가 후 input-box 공백
     saveData(); // 할 일을 추가할때마다 saveData 함수를 호출
-}
+};
 
 listContainer.addEventListener(
     'click',
-    function (e) {
+    e => {
         // li 를 클릭한 경우 클릭한 위치를 확인
         if (e.target.tagName === 'LI') {
             e.target.classList.toggle('checked'); // 클릭된 li 의 checked 클래스를 추가
@@ -33,12 +33,12 @@ listContainer.addEventListener(
     false
 );
 
-function saveData() {
+const saveData = () => {
     localStorage.setItem('data', listContainer.innerHTML); // list-container 안에 html 을 브라우저 로컬 스토리지에 data 저장
-}
+};
 
-function showTask() {
+const showTask = () => {
     listContainer.innerHTML = localStorage.getItem('data'); // 로컬 스토리지에 저장된 data 를 list-container 안에 추가
-}
+};
 
 showTask(); // showTask 함수를 호출
